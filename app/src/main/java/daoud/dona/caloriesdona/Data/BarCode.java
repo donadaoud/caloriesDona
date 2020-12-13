c1827f4a0ffe334095ed49f3d67e59db03cf24cd
Owner <noran@danon.tzafonet.org.il>
2020-12-13 12:40:57 +0200

    ...

---
+++ app/src/main/java/daoud/dona/caloriesdona/Data/BarCode.java
@@ -0,0 +1,16 @@
+ package daoud.dona.caloriesdona.Data;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import daoud.dona.caloriesdona.R;
+
+ public class BarCode extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_code);
+    }
+}
